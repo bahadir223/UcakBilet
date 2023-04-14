52fb0ba672f45fdc74120262fb9a78a72f012aa4
Bahadır Gunuvar <bahadir.223@hotmail.com>
2023-04-14 23:06:59 +0300

    UcakBiletProjesi

---
+++ src/main/java/ucakprojesi/ucakprojesi/Runner.java
@@ -0,0 +1,19 @@
+package ucakprojesi.ucakprojesi;
+
+
+import static ucakprojesi.ucakprojesi.Methods.menu;
+
+public class Runner{
+
+    public static void main(String[] args) {
+
+
+        menu();
+
+
+    }
+
+
+
+
+}
